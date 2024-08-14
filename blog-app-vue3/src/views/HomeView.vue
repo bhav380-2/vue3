1f953f127d7b68595e4324fdc35a730286ff2fdc
bhav380 <bhaweshrathour18@gmail.com>
2024-08-14 14:30:16 +0530

    template refs

--- blog-app-vue3/src/views/HomeView.vue
+++ blog-app-vue3/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
-    <p> My name is {{ name }} and my age is {{ 22 }}</p>
+    <p ref="para"> My name is {{ name }} and my age is {{ 22 }}</p>
     <button @click="handleClick"> click me</button>
   </div>
 </template>
 
 <script>
+
+import {ref} from 'vue';
 // @ is an alias to /src
 
 export default {
@@ -16,15 +18,20 @@ export default {
     let name = 'mario';
     let age = 30;
 
+    const para = ref(null);
+
     const handleClick = ()=>{
-      console.log('you clicked me')
+      console.log(para,para.value)
+      para.value.classList.add('test');
+      para.value.textContent= "hello, ninjas"
     }
 
 
     return {
       name:name,
       age: age,
-      handleClick
+      handleClick,
+      para
       
     }
 

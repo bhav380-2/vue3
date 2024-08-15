b007f272be98acae8925eca56b907e587ee6b46d
bhav380 <bhaweshrathour18@gmail.com>
2024-08-15 13:29:40 +0530

    composable functions in components

--- blog-app-vue3/src/views/HomeView.vue
+++ blog-app-vue3/src/views/HomeView.vue
@@ -6,7 +6,7 @@
     <PostList  v-if="posts.length"  :posts="posts" />
     <div v-else>Loading...</div>
 
-    <button @click="showPosts=!showPosts"> toggle Posts</button>
+    <!-- <button @click="showPosts=!showPosts"> toggle Posts</button> -->
     <button @click="posts.pop()">delete a posts</button>
 
 
@@ -44,6 +44,7 @@
 <script>
 
 import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
 
 import { watch, watchEffect } from 'vue';
 import { ref, reactive, computed } from 'vue';
@@ -55,29 +56,11 @@ export default {
   components : {PostList},
 
   setup() {
-    const posts = ref([]);
-    const error = ref(null)
 
-    const load = async()=>{
-      try{
-
-        let data = await fetch('http://localhost:3000/posts')
-
-        if(!data.ok){
-          throw Error('no data available');
-        }
-
-        posts.value = await data.json();
-
-      }catch(err){
-        error.value = err.message;
-        console.log(error.value);
-
-      }
-    }
-
+    const {posts,error,load} = getPosts();
     load();
 
+   
     const showPosts = ref(true);
     return { posts ,showPosts,error};
   }
